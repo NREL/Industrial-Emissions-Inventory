28c09d6399c38fb8f4473d1938c098b6909e765e
Ghosh, Tapajyoti <tapajyotighosh007@gmail.com>
2023-03-14 06:23:30 -0600

    FECM inventory github code upload

---
+++ run.py
@@ -0,0 +1,49 @@
+'''
+This file runs the fecm inventory for sectors listed sequentially and creates output files
+in folders with current date as names. 
+The fecm inventory builder involves 3 main parts - 
+1. Running Stewi and obtaining the inventories
+2. Compiling the data to create the emissions inventory for a certain sector, find errors and provide results files at facility and process level
+3. Statistical analysis
+'''
+
+
+
+from fecm_data_explorationv2 import main
+
+
+sectors = ['cement','steel','ammonia','hydrogen','refining','natural_gas_processing']
+
+
+'''
+The sectors listed here do not have a user defined NAICS code
+Instead the NAICS code is obtained from the GHGRP Flight tool sector wise exported files and used for compilation of emission inventories
+The two boolean flags refer to 
+1. If a GHGRP Flight Tool exported file with NAICS code is available or not
+2. If a corrected parquet file is available with manual corrections provided for debugging various inventory issues and errors
+If a corrected parquet is available, the inventory builder drops all non GHGRP listed facilities
+'''
+
+
+flag_for_running_stewi = False
+
+count = 0
+for sec in sectors:
+    if count > 0:
+        main(sec,True,0,True,False)
+    else:
+        main(sec,True,0,True,flag_for_running_stewi)
+    count = count + 1
+    
+
+sectors = ['ethanol','pulp']
+'''
+For ethanol and pulp, GHGRP FLight Tool exported sector files are not available. Thus best possible NAICS code estimates are provided to build the inventory
+'''
+for sec in sectors:
+    if sec == "ethanol":
+        main(sec,False,325193,True,False)
+        pass
+    elif sec == "pulp":
+        main(sec,False,322110,True,False)
+
